20cb79eeec2c707f23fb1c1465df5b4dd6f238da
Bemourn <70115233+koenib@users.noreply.github.com>
2022-01-15 19:35:08 -0700

    allow viewing of saved notes; cleanup

--- helpers/uuid.js
+++
@@ -1,5 +0,0 @@
-// generate random alpha-numeric strings for storing unique notes objects
-module.exports = () =>
-    Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
